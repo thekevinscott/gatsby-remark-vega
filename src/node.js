03c27cfe860075b0e74234375975d53c0a44af70
Kevin Scott <kevin@hitheory.com>
2018-05-26 10:50:40 -0400

    Fix imports and exports

---
+++ src/node.js
@@ -0,0 +1,36 @@
+const visit = require('unist-util-visit');
+
+const getPageDir = ({ parent }) => parent.split(" ").shift().split("/").slice(0, -1).join("/");
+const escape = txt => txt.replace(/"/g, '&quot;');
+
+const MARKDOWN_TAG = "vega";
+
+module.exports = (
+  {
+    markdownNode,
+    markdownAST,
+  },
+) => {
+  visit(markdownAST, `inlineCode`, node => {
+    const { value } = node;
+
+    if (value.startsWith(`${MARKDOWN_TAG}:`)) {
+      const dir = getPageDir(markdownNode);
+      const file = value.split(':').pop();
+      const path = `${dir}/${file}`;
+
+      console.log('require fs!');
+      const fs = require(`fs`);
+      if (!fs.existsSync(path)) {
+        throw Error(`Invalid file specified; no such file "${path}"`);
+      }
+
+      const spec = fs.readFileSync(path, `utf8`);
+
+      node.type = `html`;
+      node.value = `<vega spec="${escape(spec)}" />`;
+    }
+  })
+
+  return markdownAST;
+};
